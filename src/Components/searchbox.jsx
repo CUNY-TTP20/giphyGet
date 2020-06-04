50977c06e2cabe5674518fd33ef39045ff076c45
theomgxz <sg28r8@gmail.com>
2020-06-04 13:17:07 -0400

    added styling

--- src/Components/searchbox.jsx
+++ src/Components/searchbox.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import aKey from "./api.json";
 import classes from "./css/main.module.css";
 
+// const API = process.env.API_KEY;
 const API = aKey.map((data) => {
   return data.API;
 });
@@ -30,44 +31,87 @@ class SearchBox extends Component {
       "http://api.giphy.com/v1/gifs/trending?api_key=YOUR_API_KEY";
     const randomCall =
       "http://api.giphy.com/v1/gifs/random?api_key=YOUR_API_KEY";
+
     return (
-      <div className>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            <input
-              type="radio"
-              name="regular"
-              value="regular"
-              checked={regular ? true : false}
-            />
+      <div>
+        <div className={classes.buttonDiv}>
+          <button
+            className="btn btn-dark"
+            type="button"
+            name="regular"
+            value="regular"
+            checked={regular ? true : false}
+            onClick={this.handleRegular}
+          >
             Regular
-          </label>
+          </button>
           &emsp;
-          <label>
-            <input
-              type="radio"
-              name="random"
-              value="random"
-              checked={random ? true : false}
-            />
+          <button
+            className="btn btn-dark"
+            type="button"
+            name="random"
+            value="random"
+            onClick={this.handleRandom}
+          >
             Random
-          </label>
+          </button>
           &emsp;
-          <label>
-            <input
-              type="radio"
-              name="trending"
-              value="trending"
-              checked={trending ? true : false}
-            />
+          <button
+            className="btn btn-dark"
+            type="button"
+            name="trending"
+            value="trending"
+            checked={trending ? true : false}
+            onClick={this.handleTrending}
+          >
             Trending
-          </label>
-          <br />
-          <input placeholder="Lets get some memes in this place"></input>
-          &emsp;<button> Click Me </button>
-        </form>
+          </button>
+        </div>
+        <br />
+        <div className={classes.searchDiv}>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              className="form-control form-control"
+              placeholder="Lets get some memes in this place"
+            ></input>
+            <button className="btn btn-dark"> Click Me </button>
+          </form>
+        </div>
       </div>
     );
   }
+  handleRandom = (event) => {
+    if (this.state.random === false && (this.state.regular === true || this.state.trending === true)) {
+      this.setState({
+        random: true,
+        regular: false,
+        trending: false,
+      });
+    } else {
+      console.log("eeee");
+    }
+  };
+  handleRegular=(event)=> {
+    if (this.state.regular === false && (this.state.random === true || this.state.trending === true)) {
+      this.setState({
+        random: false,
+        regular: true,
+        trending: false,
+      });
+    } else {
+      console.log("eeee");
+    }
+  }
+  handleTrending = (event) => {
+    if (this.state.trending === false && (this.state.regular === true || this.state.random === true)) {
+      this.setState({
+        random: false,
+        regular: false,
+        trending: true,
+      });
+    } else {
+      console.log("eeee");
+    }
+  };
 }
 export default SearchBox;
