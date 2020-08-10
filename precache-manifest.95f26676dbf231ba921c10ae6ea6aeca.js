a319734b5debfb5e7425a72cbe2a8f4263f164d9
Sagar Shrestha <sg28r8@gmail.com>
2020-08-10 12:37:39 -0400

    Updates

---
+++ precache-manifest.95f26676dbf231ba921c10ae6ea6aeca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb508be4daccba066a46c77dabf5d51",
+    "url": "/giphyGet/index.html"
+  },
+  {
+    "revision": "73b7c91cab614fce79d9",
+    "url": "/giphyGet/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1eb16afb075ad14494f1",
+    "url": "/giphyGet/static/css/main.e357aca7.chunk.css"
+  },
+  {
+    "revision": "73b7c91cab614fce79d9",
+    "url": "/giphyGet/static/js/2.c4223926.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphyGet/static/js/2.c4223926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb16afb075ad14494f1",
+    "url": "/giphyGet/static/js/main.640dd958.chunk.js"
+  },
+  {
+    "revision": "139c989b65a66344822f",
+    "url": "/giphyGet/static/js/runtime-main.f71e04de.js"
+  }
+]);
